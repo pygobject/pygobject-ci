6a0368d7c1c47894926c24a639a45037db21c044
Christoph Reiter <reiter.christoph@gmail.com>
2017-08-26 14:01:08 +0200

    travis: try to cache docker images

--- test-linux.sh
+++ test-linux.sh
@@ -2,7 +2,15 @@
 
 set -e
 
-docker build -t myimage -f "$DOCKERFILE" .
+DOCKERIMAGE="$DOCKERFILE.image"
+
+if [ ! -f "_ci_cache/$DOCKERIMAGE" ]; then
+    docker build -t "$DOCKERIMAGE" -f "$DOCKERFILE" .
+    mkdir -p _ci_cache;
+    docker image save "$DOCKERIMAGE" -o "_ci_cache/$DOCKERIMAGE";
+fi;
+
+docker image load -i "_ci_cache/$DOCKERIMAGE"
 docker run --volume "$(pwd):/app" --workdir "/app" --tty --detach myimage bash > container_id
 docker exec "$(cat container_id)" bash -x test-docker.sh $PYTHON
 docker stop "$(cat container_id)"
