08882bdc1eeda6b0188105e57cef38151a2ea20f
Christoph Reiter <reiter.christoph@gmail.com>
2017-08-26 14:47:33 +0200

    dummy

--- test-linux.sh
+++ test-linux.sh
@@ -3,13 +3,11 @@
 set -e
 
 DOCKERFILE="Dockerfile.$DOCKERIMAGE"
-
 if [ ! -f "_ci_cache/$DOCKERIMAGE" ]; then
     docker build -t "$DOCKERIMAGE" -f "$DOCKERFILE" .
     mkdir -p _ci_cache;
     docker image save "$DOCKERIMAGE" -o "_ci_cache/$DOCKERIMAGE";
 fi;
-
 docker image load -i "_ci_cache/$DOCKERIMAGE"
 docker run --volume "$(pwd):/app" --workdir "/app" --tty --detach "$DOCKERIMAGE" bash > container_id
 docker exec "$(cat container_id)" bash -x test-docker.sh $PYTHON
