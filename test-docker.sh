74c32249233824dc5e9dad490ed42c9f85b2cea6
Christoph Reiter <reiter.christoph@gmail.com>
2017-09-14 09:12:09 +0200

    grr

--- test-docker.sh
+++ test-docker.sh
@@ -2,13 +2,16 @@
 
 set -e
 
-virtualenv --python=/usr/bin/$1 /tmp/venv
-source /tmp/venv/bin/activate
-pip install git+https://github.com/pygobject/pycairo.git
-pip install pyflakes pycodestyle
-export PKG_CONFIG_PATH=/tmp/venv/lib/pkgconfig
+ls -l
 cd pygobject
-./autogen.sh --with-python=python
-make
-xvfb-run -a make check
-make check.quality
+ls -l
+#~ virtualenv --python=/usr/bin/$1 /tmp/venv
+#~ source /tmp/venv/bin/activate
+#~ pip install git+https://github.com/pygobject/pycairo.git
+#~ pip install pyflakes pycodestyle
+#~ export PKG_CONFIG_PATH=/tmp/venv/lib/pkgconfig
+#~ cd pygobject
+#~ ./autogen.sh --with-python=python
+#~ make
+#~ xvfb-run -a make check
+#~ make check.quality
