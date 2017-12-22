992d294308647378d58d98489c3b9370853f4d34
Christoph Reiter <reiter.christoph@gmail.com>
2017-12-22 19:01:06 +0100

    Add doc checks

--- test-docker.sh
+++ test-docker.sh
@@ -2,14 +2,16 @@
 
 set -e
 
-virtualenv --python=/usr/bin/$1 /tmp/venv
+virtualenv --python="${PYTHON}" /tmp/venv
 source /tmp/venv/bin/activate
+
 pip install git+https://github.com/pygobject/pycairo.git
 pip install pyflakes pycodestyle
-export PKG_CONFIG_PATH=/tmp/venv/share/pkgconfig
 
+export PKG_CONFIG_PATH=/tmp/venv/share/pkgconfig
 export MALLOC_CHECK_=3
-export MALLOC_PERTURB_=$(($RANDOM % 255 + 1))
+export MALLOC_PERTURB_=$((${RANDOM} % 255 + 1))
+PYVER=$(python -c "import sys; sys.stdout.write(str(sys.version_info[0]))")
 
 for repo in pygobject-master pygobject-3-26;
 do
@@ -26,5 +28,11 @@ do
         LANG=C xvfb-run -a make check
     fi;
     make check.quality
+
+    if [[ "${PYVER}" == "2" ]] && [[ "${repo}" == "pygobject-master" ]]; then
+        python -m pip install sphinx sphinx_rtd_theme
+        python -m sphinx -W -a -E -b html -n docs docs/_build
+    fi;
+
     cd ..
 done
