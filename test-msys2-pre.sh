a65f07a663446fc0eae45df90f7e609cad774cd0
Christoph Reiter <reiter.christoph@gmail.com>
2018-02-10 09:59:36 +0100

    Update test-msys2-pre.sh

--- test-msys2-pre.sh
+++ test-msys2-pre.sh
@@ -4,6 +4,4 @@ set -e
 
 sed -i 's/^CheckSpace/#CheckSpace/g' /etc/pacman.conf
 
-pacman --noconfirm -Rdd catgets || true
-pacman --noconfirm -Rdd libcatgets || true
-pacman --noconfirm --sync --refresh --refresh --sysupgrade --sysupgrade
+pacman --noconfirm --sync --refresh --refresh --sysupgrade --sysupgrade --ask 20
