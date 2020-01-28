25ac05d90dc6fdcfc3eb33b78a9a2b9e3f349af1
Christoph Reiter <reiter.christoph@gmail.com>
2020-01-28 21:18:33 +0100

    Stop testing with Python 2

--- test-pygobject.sh
+++ test-pygobject.sh
@@ -9,15 +9,9 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 brew update
 brew remove --ignore-dependencies gdal numpy postgis
 
-if [ "$PYTHON" == "python2" ]; then
-    brew unlink python || true;
-    brew install "python@2" || brew upgrade "python@2";
-    export PATH="/usr/local/opt/python@2/libexec/bin:$PATH"
-else
-    brew unlink python@2 || true;
-    brew install "python" || brew upgrade "python";
-    export PATH="/usr/local/opt/python/libexec/bin:$PATH"
-fi
+brew unlink python@2 || true;
+brew install "python" || brew upgrade "python";
+export PATH="/usr/local/opt/python/libexec/bin:$PATH"
 
 for package in libffi glib cairo gobject-introspection gtk+3; do
     brew install "$package" || brew upgrade "$package";
@@ -28,11 +22,10 @@ export PKG_CONFIG_PATH="/usr/local/opt/libffi/lib/pkgconfig"
 # https://bugzilla.gnome.org/show_bug.cgi?id=780238#c4
 export ARCHFLAGS="-arch x86_64"
 
-$PYTHON --version
-$PYTHON -m pip install git+https://github.com/pygobject/pycairo.git
-$PYTHON -m pip install flake8 pytest pytest-faulthandler
+python3 --version
+python3 -m pip pycairo install flake8 pytest pytest-faulthandler
 
 git clone --depth 1 https://gitlab.gnome.org/GNOME/pygobject.git
 cd pygobject
 
-"$PYTHON" setup.py distcheck
+python3 setup.py distcheck
